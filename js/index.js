3d146e9a9d4113ac1198f2bd7ae16028940453a9
Volodymyr <volodymyr.khvesyk@gmail.com>
2017-03-06 02:07:46 +0000

    swipe rem

--- js/index.js
+++ js/index.js
@@ -86,24 +86,6 @@ $(document).ready(function() {
 	$('.carousel').carousel({
 		interval: 7000,
 		pause: false
-	});
-	
-	$(".carousel").swiperight(function() {
-    	$(this).carousel('prev');
-	});
-		
-	$(".carousel").swipeleft(function() {
-		$(this).carousel('next');
-	});
-	
-	$(".carousel-inner").swipe( {
-		swipeLeft:function(event, direction, distance, duration, fingerCount) {
-			$(this).parent().carousel('prev'); 
-		},
-		swipeRight: function() {
-			$(this).parent().carousel('next'); 
-		},
-		threshold:0
 	})
 	
 	
