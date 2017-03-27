5576a2437d70f67c99e6928a5de18117ba6e8cf2
Volodymyr <volodymyr.khvesyk@gmail.com>
2017-03-27 03:02:11 +0000

    some adds & canvas dots

--- js/index.js
+++ js/index.js
@@ -49,12 +49,13 @@ $(document).ready(function() {
 
 	});
     
- //   $('.player').on('click', '#muted', function(e) {
-	// 	e.preventDefault();
-	// 	song.volume = curVolume / 100;;
-	// 	$(this).replaceWith('<a class="play-btn gradient" id="mute" href="" title=""><i class="fa fa-volume-up"></a>');
+    $('.player').on('click', '#muted', function(e) {
+		e.preventDefault();
+		theVolume = vol.val()
+		song.volume = theVolume / 100;
+		$(this).replaceWith('<a class="play-btn gradient" id="mute" href="" title=""><i class="fa fa-volume-up"></a>');
 
-	// });
+	});
     
     $('.player').on('click', '#close', function(e) {
 		e.preventDefault();
@@ -76,20 +77,11 @@ $(document).ready(function() {
 	$("#seek").attr("value", curtime);
 	});
 	
-	// function getMuted(color, qty) {
-	//     $('.player').on('click', '#muted', function(e) {
-	// 		e.preventDefault();
-	// 		song.volume = curVolume / 100;;
-	// 		$(this).replaceWith('<a class="play-btn gradient" id="mute" href="" title=""><i class="fa fa-volume-up"></a>');
 	
-	// 	});
- //   };
-
 	$('.player').on('change', '#volume', function(e) {
 		e.preventDefault();
-		// alert( this.value );
-    	curVolume = this.value;
-    	song.volume = curVolume / 100;
+		curVolume = this.value;
+		song.volume = curVolume / 100;
 	});
 	
 
@@ -192,7 +184,7 @@ $(document).ready(function() {
 			    $(".alert").alert('hide');
 			});
 		}
-	}
+	};
 	
 	
 	
@@ -218,8 +210,108 @@ $(document).ready(function() {
 	//     }
 	// });
 	
+	var canvasDots = function() {
+		var canvas = document.querySelector('canvas'),
+			ctx = canvas.getContext('2d'),
+			colorDot = '#ccffff',
+			color = '#ccffff';
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		canvas.style.display = 'block';
+		ctx.fillStyle = colorDot;
+		ctx.lineWidth = .1;
+		ctx.strokeStyle = color;
+		
+		var mousePosition = {
+			x: 60 * canvas.width / 100,
+			y: 60 * canvas.height / 100
+		};
+	
+		var dots = {
+			nb: 550,
+			distance: 60,
+			d_radius: 100,
+			array: []
+		};
+		
+		function Dot(){
+			this.x = Math.random() * canvas.width;
+			this.y = Math.random() * canvas.height;
+		
+			this.vx = -.5 + Math.random();
+			this.vy = -.5 + Math.random();
+			
+			this.radius = Math.random();
+		}
+		
+		Dot.prototype = {
+			create: function(){
+				ctx.beginPath();
+				ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+				ctx.fill();
+			},
+			animate: function(){
+				for(i = 0; i < dots.nb; i++){
+					var dot = dots.array[i];
+					if(dot.y < 0 || dot.y > canvas.height){
+						dot.vx = dot.vx;
+						dot.vy = - dot.vy;
+					}
+					else if(dot.x < 0 || dot.x > canvas.width){
+						dot.vx = - dot.vx;
+						dot.vy = dot.vy;
+					}
+					dot.x += dot.vx;
+					dot.y += dot.vy;
+				}
+			},
+			
+			line: function(){
+				for(i = 0; i < dots.nb; i++){
+					for(j = 0; j < dots.nb; j++){
+						i_dot = dots.array[i];
+						j_dot = dots.array[j];
+						
+						if((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > - dots.distance && (i_dot.y - j_dot.y) > - dots.distance){
+							if((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > - dots.d_radius && (i_dot.y - mousePosition.y) > - dots.d_radius){
+								ctx.beginPath();
+								ctx.moveTo(i_dot.x, i_dot.y);
+								ctx.lineTo(j_dot.x, j_dot.y);
+								ctx.stroke();
+								ctx.closePath();
+							}
+						}
+					}
+				}
+			}
+		};
+		
+		function createDots(){
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			for(i = 0; i < dots.nb; i++){
+				dots.array.push(new Dot());
+				dot = dots.array[i];
+				dot.create();
+			}
+			dot.line();
+			dot.animate();
+		}
+		
+		window.onmousemove = function(parameter) {
+			mousePosition.x = parameter.pageX;
+			mousePosition.y = parameter.pageY;
+		}
+		
+		mousePosition.x = window.innerWidth / 2;
+		mousePosition.y = window.innerHeight / 2;
+		
+		setInterval(createDots, 2000/60); 
+	};
 
-
+	window.onload = function() {
+		canvasDots();
+	}
+	
 	
 });
 
